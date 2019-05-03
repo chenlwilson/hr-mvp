c5db9131d44b58cf9258b573ee4bd09b1c48d9f5
Chen Wilson <chenlwilson@gmail.com>
2019-05-02 23:37:24 -0700

    can render svg polyline

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,11 @@ export default class App extends Component {
     this.state = {
       isDrawing: false,
       countdown: 9,
-      vector: [],
+      coordinates: [[
+        [149, 15], [122, 13], [97, 17], [72, 32], [59, 43], [35, 74], [26, 92], [18, 127], [17, 164], [21, 181], [29, 196],
+        [40, 208], [66, 225], [80, 230], [113, 233], [132, 233], [153, 227], [186, 203], [213, 168], [231, 129], [233, 95],
+        [228, 79], [203, 44], [164, 16], [150, 14],
+      ]],
     };
   }
 
@@ -45,7 +49,8 @@ export default class App extends Component {
     return (
       <div>
         { this.state.isDrawing
-          ? <Canvas stopGame={this.stopGame} countdown={this.state.countdown} /> : <Cover startGame={this.startGame} /> }
+          ? <Canvas stopGame={this.stopGame} countdown={this.state.countdown} coordinates={this.state.coordinates} />
+          : <Cover startGame={this.startGame} /> }
       </div>
     );
   }
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { Navbar, Button, Badge } from 'react-bootstrap';
 
-const Canvas = ({ countdown, stopGame }) => (
+const Canvas = ({ countdown, stopGame, coordinates }) => (
   <div>
     <Navbar bg="light">
       <Button variant="outline-warning" onClick={stopGame}>Restart</Button>
-      <h2><Badge bg="light" variant="warning">00:0{countdown}</Badge></h2>
+      <Button bg="light" variant="warning">00:0{countdown}</Button>
     </Navbar>
-    {/* <canvas
-          id="drawingCanvas"
-          resize="true"
-          width="2022"
-          height="360"
-          style="-webkit-user-drag: none; user-select: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0); width: 2022px; height: 360px;"
-        /> */}
+    <svg>
+      {coordinates.map(vector => <polyline points={vector.join(' ')} />)}
+    </svg>
   </div>
 );
 
