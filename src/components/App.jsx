ba8f637be31b5216826439b02aec5382b4535adb
Chen Wilson <chenlwilson@gmail.com>
2019-05-03 00:09:44 -0700

    Added mousedown and mouseup for drawing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,20 +7,18 @@ export default class App extends Component {
     super(props);
     this.startGame = this.startGame.bind(this);
     this.stopGame = this.stopGame.bind(this);
+    this.startPolyline = this.startPolyline.bind(this);
+    this.stopPolyline = this.stopPolyline.bind(this);
 
     this.state = {
       isDrawing: false,
-      countdown: 9,
+      countdown: 9999,
       coordinates: [[
         [149, 15], [122, 13], [97, 17], [72, 32], [59, 43], [35, 74], [26, 92], [18, 127], [17, 164], [21, 181], [29, 196],
         [40, 208], [66, 225], [80, 230], [113, 233], [132, 233], [153, 227], [186, 203], [213, 168], [231, 129], [233, 95],
         [228, 79], [203, 44], [164, 16], [150, 14],
       ]],
     };
-  }
-
-  componentDidMount() {
-
   }
 
   startGame() {
@@ -45,11 +43,35 @@ export default class App extends Component {
     clearInterval(this.timer);
   }
 
+  startPolyline(e) {
+    const x = e.clientX;
+    const y = e.clientY;
+    this.setState({
+      coordinates: [...this.state.coordinates, [[x, y]]],
+    });
+  }
+
+  stopPolyline(e) {
+    const x = e.clientX;
+    const y = e.clientY;
+    const lastLine = this.state.coordinates[this.state.coordinates.length - 1];
+    console.log(lastLine);
+    const newLastLine = [...lastLine, [x, y]];
+    console.log(newLastLine);
+    this.setState({
+      coordinates: [...this.state.coordinates, newLastLine],
+    });
+  }
+
+  continuePolyline(e) {
+    
+  }
+
   render() {
     return (
       <div>
         { this.state.isDrawing
-          ? <Canvas stopGame={this.stopGame} countdown={this.state.countdown} coordinates={this.state.coordinates} />
+          ? <Canvas stopGame={this.stopGame} countdown={this.state.countdown} coordinates={this.state.coordinates} startPolyline={this.startPolyline} stopPolyline={this.stopPolyline} />
           : <Cover startGame={this.startGame} /> }
       </div>
     );
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Navbar, Button, Badge } from 'react-bootstrap';
 
-const Canvas = ({ countdown, stopGame, coordinates }) => (
+const Canvas = ({ countdown, stopGame, coordinates, startPolyline, stopPolyline }) => (
   <div>
     <Navbar bg="light">
       <Button variant="outline-warning" onClick={stopGame}>Restart</Button>
       <Button bg="light" variant="warning">00:0{countdown}</Button>
     </Navbar>
-    <svg>
-      {coordinates.map(vector => <polyline points={vector.join(' ')} />)}
+    <svg
+      onMouseDown={(e) => { startPolyline(e); }}
+      onMouseUp={(e) => { stopPolyline(e); }}
+    >
+      {coordinates.map(vector => <polyline points={vector.join(' ')} key={vector[0].join(',')} />)}
     </svg>
   </div>
 );
